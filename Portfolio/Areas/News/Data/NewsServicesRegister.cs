e43accc0de11a6b42ebedde4a19d34491179512e
Виталий Крюков <mr.vitalik.kr@yandex.ru>
2022-08-04 12:37:42 +0500

    ReadController return Post by ID

---
+++ Portfolio/Areas/News/Data/NewsServicesRegister.cs
@@ -0,0 +1,12 @@
+﻿using Portfolio.Areas.News.Data.Post;
+
+namespace Portfolio.Areas.News.Data
+{
+    public static class NewsServicesRegister
+    {
+        public static void AddNewsServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPostRepository, PostRepository>();
+        }
+    }
+}
---
+++ Portfolio/Areas/News/Data/Post/IPostRepository.cs
@@ -0,0 +1,10 @@
+﻿using Portfolio.Areas.News.Models.Post;
+
+namespace Portfolio.Areas.News.Data.Post
+{
+    public interface IPostRepository
+    {
+        public void AddPost(PostModel post);
+        public PostModel? GetPostWithAuthor(int Id);
+    }
+}
