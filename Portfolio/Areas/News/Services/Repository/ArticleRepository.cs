0a43c30d85102f83e40ccbf65af2443b1ae07fff
Виталий Крюков <mr.vitalik.kr@yandex.ru>
2022-08-28 10:40:21 +0500

    Optimized article requests to DB

--- Portfolio/Areas/News/Services/Repository/ArticleRepository.cs
+++ Portfolio/Areas/News/Services/Repository/ArticleRepository.cs
@@ -16,25 +16,43 @@ namespace Portfolio.Areas.News.Services.Repository
 
         public async Task<Article?> GetByIdAsync(int Id)
         {
-            return await _database.Articles.FirstOrDefaultAsync(a => a.Id == Id);
+            return await _database.Articles.AsNoTracking().FirstOrDefaultAsync(a => a.Id == Id);
         }
 
         public async Task<IEnumerable<Article>> GetRecentlyAsync(int count)
         {
-            return await _database.Articles
+            var res = await _database.Articles.AsNoTracking()
                 .Where(a => a.IsDeleted == false && a.IsPubleshed)
                 .OrderByDescending(a => a.Id)
-                .Take(count).ToArrayAsync();
+                .Take(count).Select(a => new { a.Id, a.Title, a.Description }).ToArrayAsync();
+
+            Article[] articles = new Article[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                articles[0] = new()
+                {
+                    Id = res[i].Id,
+                    Title = res[i].Title,
+                    Description = res[i].Description,
+                };
+            }
+            return articles;
         }
 
         public async Task<Article?> GetSpotlight()
         {
-            return await _database.Articles.Where(c => c.Id == _database.ArticleViewers
-                .Select(av => new { av.ArticleId, av.ViewTime })
-                .Where(av => av.ViewTime.Hour >= DateTime.UtcNow.Hour - 1)
-                .GroupBy(av => av.ArticleId).Select(a => new { a.Key, Count = a.Count() })
-                .OrderByDescending(a => a.Count).FirstOrDefault()!.Key
-            ).FirstOrDefaultAsync();
+            var res = await _database.Articles.AsNoTracking()
+                .Where(c => c.Id == _database.ArticleViewers
+                    .Select(av => new { av.ArticleId, av.ViewTime })
+                    .Where(av => av.ViewTime.Hour >= DateTime.UtcNow.Hour - 1)
+                    .GroupBy(av => av.ArticleId).Select(a => new { a.Key, Count = a.Count() })
+                    .OrderByDescending(a => a.Count).FirstOrDefault()!.Key
+            ).Select(a => new { a.Id, a.Title, a.Description, a.DateOfPublishing, a.AuthorId }).FirstOrDefaultAsync();
+
+            if (res == null) return null;
+
+            return new() { Id = res.Id, Title = res.Title, Description = res.Description, AuthorId = res.AuthorId };
         }
 
         public async Task AddArticleAsync(Article model)
@@ -51,7 +69,7 @@ namespace Portfolio.Areas.News.Services.Repository
 
         public async Task<IEnumerable<Article>> GetArticlesByAuthorIdAsync(string authorId)
         {
-            return await _database.Articles
+            return await _database.Articles.AsNoTracking()
                 .Where(a => a.AuthorId == authorId)
                 .ToArrayAsync();
         }
@@ -71,7 +89,7 @@ namespace Portfolio.Areas.News.Services.Repository
         }
         public async Task<IEnumerable<ArticleViewers>> GetUserHistory(string userId)
         {
-            return await _database.ArticleViewers
+            return await _database.ArticleViewers.AsNoTracking()
                 .Include(av => av.Article)
                 .Where(av => av.ViewerId == userId)
                 .OrderByDescending(av => av.Id)
@@ -80,13 +98,15 @@ namespace Portfolio.Areas.News.Services.Repository
 
         public async Task<AnalyticModel?> GetAnaliticsByIdAsync(int articleId, string AuthorId)
         {
-            var res = await _database.Articles.Select(c => new {
-                c.AuthorId,
-                Views = _database.ArticleViewers.Where(av=> av.Id == articleId).Count(),
-                UnicalViews = _database.ArticleViewers.Where(av => av.Id == articleId).GroupBy(av=> av.Id).Count(),
-                LastHour = _database.ArticleViewers.Where(av => av.ArticleId == articleId).Count(av => av.ViewTime.Hour >= DateTime.UtcNow.Hour - 1),
-                Today = _database.ArticleViewers.Where(av => av.ArticleId == articleId).Count(av => av.ViewTime.Day == DateTime.UtcNow.Day)
-            }).FirstOrDefaultAsync();
+            var res = await _database.Articles.AsNoTracking()
+                .Select(c => new
+                {
+                    c.AuthorId,
+                    Views = _database.ArticleViewers.Where(av => av.Id == articleId).Count(),
+                    UnicalViews = _database.ArticleViewers.Where(av => av.Id == articleId).GroupBy(av => av.Id).Count(),
+                    LastHour = _database.ArticleViewers.Where(av => av.ArticleId == articleId).Count(av => av.ViewTime.Hour >= DateTime.UtcNow.Hour - 1),
+                    Today = _database.ArticleViewers.Where(av => av.ArticleId == articleId).Count(av => av.ViewTime.Day == DateTime.UtcNow.Day)
+                }).FirstOrDefaultAsync();
 
             if (res == null || res.AuthorId != AuthorId)
             {
--- Portfolio/Services/Repository/MessageRepository.cs
+++ Portfolio/Services/Repository/MessageRepository.cs
@@ -1,4 +1,5 @@
-﻿using Portfolio.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Models;
 using Portfolio.Models.StartPage;
 
 namespace Portfolio.Services.Repository
@@ -16,6 +17,12 @@ namespace Portfolio.Services.Repository
         {
             _database.Messages.Add(message);
             await _database.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<MessageModel>> Get(int page = 0, int count = 10)
+        {
+            return await _database.Messages.AsNoTracking()
+                .Take(count).Skip(page * count).ToListAsync();
         }
     }
 }
